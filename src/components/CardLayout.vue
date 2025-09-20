870c6c26b35559b2642b04e39275410d1a1754b0
Blake Teres <blake@twopalms.io>
2025-09-20 17:56:52 -0400

    add placeholder

--- src/components/CardLayout.vue
+++ src/components/CardLayout.vue
@@ -27,5 +27,8 @@ watch(
     :cardList="cardList"
     :class="`${isRoot() ? '' : 'hidden'} sm:block`"
   />
+  <div v-if="activeCardIndex === null" class="flex flex-1 justify-center items-center text-2xl">
+    <h4>Select a card to get started</h4>
+  </div>
   <router-view />
 </template>
